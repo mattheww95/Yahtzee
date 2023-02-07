1d496218618c1845ed40fc702263e15fcc165df8
Matthew Wells <mattwells9@shaw.ca>
2023-02-06 22:03:15 -0600

    updated player funcs

--- src/main.lisp
+++ src/main.lisp
@@ -4,9 +4,26 @@
 
 (defconstant +dice-sides+ 6 "The number of sides for the dice to be rolled")
 (defparameter *dice* `((1 . nil) (2 . nil) (3 . nil) (4 . nil) (5 . nil) (6 . nil)))
+(defparameter *score* `((Aces . 0)
+                        (Twos . 0)
+                        (Threes . 0)
+                        (Fours . 0)
+                        (Sixes . 0)
+                        (Three-of-a-kind . 0)
+                        (Four-of-a-kind . 0)
+                        (Full-House . 0)
+                        (Small-Straight . 0)
+                        (Large-Straight . 0)
+                        (Yahtzee . 0)
+                        (Chance . 0)))
+
+(defvar *players* () "Store each of the players")
+(defvar *number-players* 0)
 
 (setf *random-state* (make-random-state t))
 
+
+;;;  Dice functions
 
 (defun get-random-value (mod-val)
   "return a random value between 1 and the mod-val"
@@ -45,3 +62,35 @@
   (mapcar #'print-dice *dice*))
 
 (print-all-dice)
+
+
+;;; Player functions
+
+(defun create-player ()
+  "Create a new player for the game"
+  (copy-tree *score*))
+
+(defun add-player ()
+  "Add a new player"
+  (setf *number-players* (1+ *number-players*))
+  (setf *players* (cons (cons *number-players* (create-player)) *players*)))
+
+;; Creating a players first move
+(defun player-turn (player)
+  "Get a player and begin their turn"
+  (let ((p (assoc player *players*))
+        (vals (loop :for i :from 1 :below (+ 1 +dice-sides+) :collect i)))
+    (progn
+      (print-all-dice)
+      (apply #'roll-select-dice vals)
+      (print-all-dice))))
+;; Need to collect user input in a turn
+    
+
+(player-turn 1)
+(print *players*)
+(add-player)
+
+(print ())
+(setf *players* ())
+(setf *number-players* 0)
